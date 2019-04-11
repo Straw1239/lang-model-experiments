f1a9940e8d0512d493cb5daab7607b830d36264c
Rabbidon <edwinfennell1@gmail.com>
2019-04-11 17:23:25 +0100

    Implemented basic net structure that works using causal CNNs
    Signed-off-by: Rabbidon <edwinfennell1@gmail.com>

--- src/main/ConvLM.py
+++ src/main/ConvLM.py
@@ -3,21 +3,42 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class ShiftedConv1D(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, dilation, groups):
-        super(ShiftedConv1D, self).__init__()
-        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size, padding=dilation*(kernel_size-1), dilation=dilation, groups=groups)
+class CausalConv1d(nn.Conv1d):
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 kernel_size,
+                 stride=1,
+                 dilation=1,
+                 groups=1,
+                 bias=True):
+        super(CausalConv1d, self).__init__(
+            in_channels,
+            out_channels,
+            kernel_size,
+            stride=stride,
+            padding=0,
+            dilation=dilation,
+            groups=groups,
+            bias=bias)
 
-    def forward(self, x):
-        return self.conv(x)
+        self.left_padding = dilation * (kernel_size - 1)
+
+    def forward(self, input):
+        x = F.pad(input.unsqueeze(2), (self.left_padding, 0, 0, 0)).squeeze(2)
+
+        return super(CausalConv1d, self).forward(x)
 
 class ConvLM(nn.Module):
     def __init__(self):
         super(ConvLM,self).__init__()
-        self.conv1 = nn.Conv1d(10, 100, 3)
-        self.conv2 = nn.Conv1d(110, 256, 3)
+        self.embd = nn.Embedding(256, 10)
+        self.conv1 = CausalConv1d(10, 1000, 3)
+        self.conv2 = CausalConv1d(1010, 256, 3)
 
     def forward(self, x):
+
+        x = self.embd(x).permute(0,2,1)
         l1out = F.selu(self.conv1(x))
-        x = self.conv2(torch.cat(x, l1out))
+        x = self.conv2(torch.cat((x, l1out),1))
         return x
--- src/main/main.py
+++ src/main/main.py
@@ -1,7 +1,37 @@
 import ConvLM
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import random as rand
 
-# Build model
+def train(model, dataset, iterations, block_size):
 
-model = ConvLM.ConvLM()
+    model = model.cuda()
+    criterion = nn.CrossEntropyLoss()
+    opt = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-# Train
+    running_loss = 0.0
+
+    for i in range(iterations):
+
+        r = rand.randint(0,len(dataset)-block_size-1)
+        inputs = dataset[r : r+block_size].unsqueeze(0).cuda()
+        labels = dataset[r+1 : r+block_size+1].unsqueeze(0).cuda()
+
+        opt.zero_grad()
+
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        opt.step()
+
+        running_loss += loss.item()
+        if i % 100 == 99:
+            print(running_loss)
+            running_loss = 0.0
+
+cheesestring = "cheese"*10000
+cheesetensor = torch.tensor([ord(c) for c in cheesestring])
+
+train(ConvLM.ConvLM(), cheesetensor, 100000, 2000)
+
